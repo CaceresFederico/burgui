7fee4bdc6fdb576f469e4c37d839a0a055b164e4
Federico <Caceresfederico@hotmail.com>
2024-01-30 01:05:16 -0300

    Small device mobile responsive enhanced

--- README.md
+++
@@ -1,8 +0,0 @@
-Burger-web
-Own project for my portfolio in colaboration with Dennis Carmona Design
-
-Features: -Desktop First -Responsive static web -Good ux/ui
-
-Techologies: FIGMA, HTML5, CSS3, BOOTSTRAP, JS
-
-DESIGN BY: DENNIS CARMONA CODE BY : FEDERICO CACERES
--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-const paternal = document.getElementById('paternal')
-const palermo = document.getElementById('palermo')
-const laBoca = document.getElementById('laBoca')
-const devoto = document.getElementById('devoto')
-const menuContent = document.getElementById('menu-content')
-const foundUsData = document.getElementById('found-us-data')
+const paternal = document.getElementById("paternal");
+const palermo = document.getElementById("palermo");
+const laBoca = document.getElementById("laBoca");
+const devoto = document.getElementById("devoto");
+const menuContent = document.getElementById("menu-content");
+const foundUsData = document.getElementById("found-us-data");
 
-paternal.addEventListener('mouseover', () => {
-    paternal.textContent = "AV.JUAN B JUSTO 5122 +54 1189741233"
-    setTimeout(() => paternal.textContent = 'PATERNAL', 5000)
-})
-palermo.addEventListener('mouseover', () => {
-    palermo.textContent = "AV. SANTA FE 3323 +54 1189741233";
-    setTimeout(() => palermo.textContent = 'PALERMO', 5000)
-})
-laBoca.addEventListener('mouseover', () => {
-    laBoca.textContent = "AV. CAMINITO 2020 +54 1189741233";
-    setTimeout(() => laBoca.textContent = 'LA BOCA', 5000)
-})
-devoto.addEventListener('mouseover', () => {
-    devoto.textContent = "AV. MOSCONI 1292 +54 1189741233";
-    setTimeout(() => devoto.textContent = 'DEVOTO', 5000)
-})
+// SHOW SUCURSALES INFORMATION
+paternal.addEventListener("mouseover", () => {
+  paternal.innerHTML = `
+  <span>AV. JUAN B. JUSTO 5122</span>
+  <span class="text-yellow">+54 1189741233</span>
+  `;
+  setTimeout(() => (paternal.textContent = "PATERNAL"), 3000);
+});
+palermo.addEventListener("mouseover", () => {
+  palermo.innerHTML = `
+  <span>AV.SANTA FE 3323</span> 
+  <span class="text-yellow">+54 1189741233</span>
+  `;
+  setTimeout(() => (palermo.textContent = "PALERMO"), 3000);
+});
+laBoca.addEventListener("mouseover", () => {
+  laBoca.innerHTML = `
+  <span>AV. CAMINITO 2020</span>
+  <span class="text-yellow">+54 1189741233</span>
+  `;
+  setTimeout(() => (laBoca.textContent = "LA BOCA"), 3000);
+});
+devoto.addEventListener("mouseover", () => {
+  devoto.innerHTML = `
+  <span>AV. MOSCONI 1292</span>
+  <span class="text-yellow">+54 1189741233</span>
+  `;
+  setTimeout(() => (devoto.textContent = "DEVOTO"), 3000);
+});
 
+// NAV & SUCURSALES SHOW AND HIDE CONTENT
 function show(id) {
-    id.style.display !== "flex" ? id.style.display = "flex" : id.style.display = "none";
+  id.style.display !== "flex"
+    ? (id.style.display = "flex")
+    : (id.style.display = "none");
 }
-
-
-
-
-
-
